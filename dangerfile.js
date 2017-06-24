7bb76a6978f9561c67c04e7af72117095264db1d
kanaabe <kanakoabe5@gmail.com>
2017-06-23 23:24:18 -0400

    Oh man..equal to

--- dangerfile.js
+++ dangerfile.js
@@ -4,6 +4,6 @@ const someoneAssigned = danger.github.pr.assignee
 if (someoneAssigned === null) {
   fail("Please assign someone to merge this PR, and optionally include people who should review.")
 }
-if (danger.github.pr.body.length > 0) {
+if (danger.github.pr.body.length === 0) {
   fail("Please describe your PR.")
 }
