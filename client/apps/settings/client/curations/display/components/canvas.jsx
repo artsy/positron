a02de6720f8fa26f48d9717c40b4bfb2c8b875da
Eve Essex <eve.essex@gmail.com>
2017-10-11 17:58:56 -0400

    canvas images component

--- client/apps/settings/client/curations/display/components/canvas.jsx
+++ client/apps/settings/client/curations/display/components/canvas.jsx
@@ -2,58 +2,15 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { Col, Row } from 'react-styled-flexboxgrid'
 
-import ImageUpload from 'client/apps/edit/components/admin/components/image_upload.coffee'
+import CanvasImages from './canvas_images.jsx'
 import CharacterLimitInput from 'client/components/character_limit/index.jsx'
 
 export default class Canvas extends React.Component {
-  renderAssets = () => {
-    const { assets } = this.props.campaign.canvas
-    const uploads = assets.map((asset, imgIndex) => {
-      return this.renderImageUpload(assets, imgIndex)
-    })
-    return uploads
-  }
-
-  renderImageUpload = (assets, imgIndex) => {
-    return (
-      <ImageUpload
-        key={'canvas-assets-' + imgIndex}
-        name='canvas.assets'
-        hasVideo
-        src={assets[imgIndex] ? assets[imgIndex].url : ''}
-        onChange={(name, url) => this.onImageInputChange(name, url, imgIndex)}
-        disabled={false} />
-    )
-  }
-
-  onSlideshowImageChange = (imgIndex, url) => {
-    const { assets } = this.props.campaign.canvas
-
-    if (imgIndex || imgIndex === 0) {
-      if (url.length) {
-        assets[imgIndex].url = url
-      } else {
-        assets.splice(imgIndex, 1)
-      }
-    } else {
-      assets.push({ url })
-    }
-    return assets
-  }
-
-  onImageInputChange = (key, value, imgIndex) => {
-    const { canvas } = this.props.campaign
-    let newValue
-    if (canvas.layout === 'slideshow') {
-      newValue = this.onSlideshowImageChange(imgIndex, value)
-    } else {
-      newValue = value.length ? [{url: value}] : []
-    }
-    this.props.onChange(key, newValue, this.props.index)
-  }
 
   render () {
     const {campaign, index, onChange} = this.props
+    const isSlideshow = campaign.canvas && campaign.canvas.layout === 'slideshow'
+
     return (
       <div className='display-admin__section--campaign'>
         <Row key={index} className='inputs--text'>
@@ -65,7 +22,7 @@ export default class Canvas extends React.Component {
                     label='Body'
                     placeholder='Body'
                     defaultValue={campaign.canvas ? campaign.canvas.headline : ''}
-                    onChange={(html) => onChange('canvas.body', html, index)}
+                    onChange={(html) => onChange('canvas.headline', html, index)}
                     html
                     limit={70} />
                 : <CharacterLimitInput
@@ -103,23 +60,12 @@ export default class Canvas extends React.Component {
             </div>
           </Col>
           <Col lg>
-            <Row key={index} className='inputs--images'>
-              <Col lg>
-                <label>Image</label>
-                {campaign.canvas.layout === 'slideshow'
-                  ? this.renderAssets(campaign, index, onChange)
-                  : this.renderImageUpload(campaign.canvas.assets || [], 0)
-                }
-              </Col>
-              <Col lg>
-                <label>Logo</label>
-                <ImageUpload
-                  name='canvas.logo'
-                  src={campaign.canvas && campaign.canvas.logo}
-                  onChange={(name, url) => onChange(name, url, this.props.index)}
-                  disabled={false} />
-              </Col>
-            </Row>
+            <CanvasImages
+              key={index}
+              campaign={campaign}
+              index={index}
+              onChange={onChange}
+              isSlideshow={isSlideshow} />
           </Col>
         </Row>
       </div>
