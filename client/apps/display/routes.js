abad68e930490b1e44f428c42bdfa261dc57be68
kanaabe <kanakoabe5@gmail.com>
2017-10-30 19:02:14 -0400

    Gets static version of fallback

--- client/apps/display/routes.js
+++ client/apps/display/routes.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/server'
 import request from 'superagent'
 import { DisplayPanel } from '@artsy/reaction-force/dist/Components/Publishing/Display/DisplayPanel'
+import { InstantArticleEmailSignup } from '@artsy/reaction-force/dist/Components/Publishing'
 import { ServerStyleSheet } from 'styled-components'
 import { DisplayQuery } from 'client/apps/display/query'
 
-const { API_URL } = process.env
+const { API_URL, FORCE_URL } = process.env
 
 export const display = (req, res, next) => {
   request
@@ -18,10 +19,18 @@ export const display = (req, res, next) => {
     const display = response.body.data.display
 
     if (!display) {
-      // Render email signup
-      res.render('fallback')
+      const InstantArticleEmailSignupComponent = React.createFactory(InstantArticleEmailSignup)
+      const sheet = new ServerStyleSheet()
+      const body = ReactDOM.renderToString(
+        sheet.collectStyles(
+          InstantArticleEmailSignupComponent({
+            signupUrl: `${FORCE_URL}/signup/editorial`
+          })
+        )
+      )
+      const css = sheet.getStyleTags()
+      res.render('fallback', { css, body })
     } else {
-
       const DisplayPanelComponent = React.createFactory(DisplayPanel)
       const sheet = new ServerStyleSheet()
       const body = ReactDOM.renderToString(
@@ -32,7 +41,6 @@ export const display = (req, res, next) => {
           })
         )
       )
-
       const css = sheet.getStyleTags()
       res.render('index', { css, body })
     }
