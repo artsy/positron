3ecc7b1a3274e818bcb5a9e194dc7059052ba877
Eve Essex <eve.essex@gmail.com>
2018-01-04 15:58:35 -0500

    vertical can be changed

--- client/apps/edit/components/admin/components/verticals_tags.jsx
+++ client/apps/edit/components/admin/components/verticals_tags.jsx
@@ -1,32 +1,75 @@
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { Col, Row } from 'react-styled-flexboxgrid'
+import Verticals from '../../../../../collections/verticals.coffee'
 
 export class AdminVerticalsTags extends Component {
-  // static propTypes = {
-  //   article: PropTypes.object,
-  //   onChange: PropTypes.func
-  // }
+  static propTypes = {
+    article: PropTypes.object,
+    onChange: PropTypes.func
+  }
 
-  // onChange = (key, value) => {
-  //   const { article, onChange } = this.props
-  //   const sponsor = article.get('sponsor') || {}
+  constructor (props) {
+    super(props)
+    const { article } = props
 
-  //   sponsor[key] = value
-  //   onChange('sponsor', sponsor)
-  // }
+    this.state = {
+      vertical: article.get('vertical') || null,
+      verticals: []
+    }
+  }
+
+  componentWillMount = () => {
+    this.fetchVerticals()
+  }
+
+  fetchVerticals = async () => {
+    new Verticals().fetch({
+      cache: true,
+      success: (verticals) => {
+        let sortedVerticals = verticals.sortBy('name')
+        this.setState({verticals: sortedVerticals})
+      }
+    })
+  }
+
+  renderVerticalsList = () => {
+    const { verticals } = this.state
+    const { article, onChange } = this.props
+    const { name } = article.get('vertical')
+
+    return verticals.map((item, index) => {
+      const isActive = item.get('name') === name
+      const activeClass = isActive ? 'avant-garde-button-black' : ''
+
+      return (
+        <button
+          key={index}
+          className={`avant-garde-button ${activeClass}`}
+          onClick={() => {
+            const vertical = isActive ? {} : item.attributes
+            onChange('vertical', vertical)
+          }}
+        >
+          {item.get('name')}
+        </button>
+      )
+    })
+  }
 
   render () {
-    // const { article } = this.props
-
     return (
-      <Row className='AdminVerticalsTags'>
+      <Row className='AdminVerticalsTags edit-admin--verticals-tags'>
+
+        <Col xs={6} className='field-group'>
+          <label>Editorial Vertical</label>
+          {this.renderVerticalsList()}
+        </Col>
+
         <Col xs={6} className='field-group'>
           <label>AdminVerticalsTags</label>
         </Col>
-        <Col xs={6} className='field-group'>
-          <label>AdminVerticalsTags</label>
-        </Col>
+
       </Row>
     )
   }
