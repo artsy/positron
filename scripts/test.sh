9ffb748087c3ffcb74fcd1393a0e28c0d0ef1430
Luc Succes <l2succes@gmail.com>
2018-02-01 10:33:00 -0500

    [Dev] fix scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,4 +6,4 @@ yarn assets
 yarn mocha $(find api -name '*.test.coffee')
 yarn mocha $(find api -name '*.test.js')
 yarn mocha $(find client -name '*.test.coffee')
-yarn jest -- --runInBand $(find client -name '*.test.js')
+yarn jest -- --runInBand $(find client -name '*.test.js') --forceExit
