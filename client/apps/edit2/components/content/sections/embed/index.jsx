9429c86ce3852125273ac8d8d305fb076aef5dd0
Eve Essex <eve.essex@gmail.com>
2018-02-12 12:10:33 -0500

    convert embed section to use redux

--- client/apps/edit2/components/content/sections/embed/index.jsx
+++ client/apps/edit2/components/content/sections/embed/index.jsx
@@ -1,62 +1,31 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
 import { Embed } from '@artsy/reaction-force/dist/Components/Publishing'
-import { EmbedControls } from './controls'
+import EmbedControls from './controls'
 
 // Embed section supports external content via iframes
 
-export class SectionEmbed extends Component {
-  static propTypes = {
-    article: PropTypes.object.isRequired,
-    editing: PropTypes.bool,
-    section: PropTypes.object.isRequired
-  }
+export const SectionEmbed = (props) => {
+  const { editing, section } = props
 
-  componentWillMount = () => {
-    const { section } = this.props
+  return (
+    <section className='SectionEmbed'>
+      {editing &&
+        <EmbedControls section={section} />
+      }
 
-    if (!section.get('layout')) {
-      section.set('layout', 'column_width')
-    }
-  }
+      {section.url
+        ? <Embed section={section} />
 
-  componentWillUnmount = () => {
-    const { section } = this.props
+        : <div className='edit-section__placeholder'>
+            Add URL above
+          </div>
+      }
+    </section>
+  )
+}
 
-    if (!this.hasUrl()) {
-      section.destroy()
-    }
-  }
-
-  hasUrl = () => {
-    const { section } = this.props
-
-    return section.get('url') && section.get('url').length
-  }
-
-  render () {
-    const {
-      article,
-      editing,
-      section
-    } = this.props
-
-    return (
-      <section className='SectionEmbed'>
-        {editing &&
-          <EmbedControls
-            articleLayout={article.layout}
-            section={section}
-          />
-        }
-        {this.hasUrl()
-          ? <Embed section={section.toJSON()} />
-
-          : <div className='SectionEmbed__placeholder'>
-              Add URL above
-            </div>
-        }
-      </section>
-    )
-  }
+SectionEmbed.propTypes = {
+  editing: PropTypes.bool,
+  section: PropTypes.object
 }
