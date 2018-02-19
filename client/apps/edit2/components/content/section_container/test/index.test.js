ae9fe0bda05c4fc2c3b3a1de865b2441189f8f9e
Eve Essex <eve.essex@gmail.com>
2018-02-19 17:35:14 -0500

    use redux components in section container

--- client/apps/edit2/components/content/section_container/test/index.test.js
+++ client/apps/edit2/components/content/section_container/test/index.test.js
@@ -1,9 +1,9 @@
+import configureStore from 'redux-mock-store'
 import React from 'react'
+import { clone } from 'lodash'
 import { mount } from 'enzyme'
-import { Fixtures, IconDrag, IconRemove } from '@artsy/reaction-force/dist/Components/Publishing'
-import Article from '/client/models/article.coffee'
-import Channel from '/client/models/channel.coffee'
-import Section from '/client/models/section.coffee'
+import { Provider } from 'react-redux'
+import { Fixtures, IconDrag } from '@artsy/reaction-force/dist/Components/Publishing'
 import { RemoveButton } from 'client/components/remove_button'
 import SectionSlideshow from '../../sections/slideshow'
 import { SectionText } from '../../sections/text/index.jsx'
@@ -12,65 +12,78 @@ import { SectionImages } from '../../sections/images'
 import { SectionVideo } from '../../sections/video'
 import { SectionContainer } from '../index'
 const { StandardArticle } = Fixtures
+require('typeahead.js')
 
 describe('SectionContainer', () => {
   let props
 
+  const getWrapper = (props) => {
+    const mockStore = configureStore([])
+
+    const store = mockStore({
+      app: {
+        channel: props.channel
+      },
+      edit: {
+        article: props.article,
+        section: props.section,
+        sectionIndex: props.index
+      }
+    })
+
+    return mount(
+      <Provider store={store}>
+        <SectionContainer {...props} />
+      </Provider>
+    )
+  }
+
   beforeEach(() => {
-    const article = new Article(StandardArticle)
+    const article = clone(StandardArticle)
 
     props = {
       article: article,
-      channel: new Channel(),
+      channel: {type: 'editorial'},
       editing: false,
       index: 1,
       isHero: false,
       onRemoveHero: jest.fn(),
       onSetEditing: jest.fn(),
-      section: article.sections.models[1],
-      sections: article.sections
+      section: {type: 'image_collection', layout: 'overflow_fillwidth'},
+      sections: article.sections,
+      removeSectionAction: jest.fn()
     }
   })
 
   it('Renders drag and remove icons', () => {
-    const component = mount(
-      <SectionContainer {...props} />
-    )
-    expect(component.find(IconRemove).length).toBe(1)
+    const component = getWrapper(props)
+    expect(component.find(RemoveButton).length).toBe(1)
     expect(component.find(IconDrag).length).toBe(1)
   })
 
   it('Calls onSetEditing with index on section click', () => {
-    const component = mount(
-      <SectionContainer {...props} />
-    )
+    const component = getWrapper(props)
     component.find('.SectionContainer__hover-controls').at(0).simulate('click')
     expect(props.onSetEditing.mock.calls[0][0]).toBe(props.index)
   })
 
   it('Calls onSetEditing with null on click off', () => {
     props.editing = true
-    const component = mount(
-      <SectionContainer {...props} />
-    )
+    const component = getWrapper(props)
 
     component.find('.SectionContainer__hover-controls').at(0).simulate('click')
     expect(props.onSetEditing.mock.calls[0][0]).toBe(null)
   })
 
   it('Can remove a section click', () => {
-    const spy = jest.spyOn(props.section, 'destroy')
-    const component = mount(
-      <SectionContainer {...props} />
-    )
+    const component = getWrapper(props)
     component.find(RemoveButton).at(0).simulate('click')
-    expect(spy).toHaveBeenCalled()
+
+    expect(props.removeSectionAction.mock.calls[0][0]).toBe(props.index)
   })
 
   it('#getContentStartEnd finds the first and last text section', () => {
-    const component = mount(
-      <SectionContainer {...props} />
-    )
+    const component = getWrapper(props).find(SectionContainer)
     const startEnd = component.instance().getContentStartEnd()
 
     expect(startEnd.start).toBe(0)
@@ -79,97 +92,75 @@ describe('SectionContainer', () => {
 
   describe('Sections', () => {
     it('Can render an embed section', () => {
-      props.section = new Section({type: 'embed'})
-      const component = mount(
-        <SectionContainer {...props} />
-      )
+      props.section = {type: 'embed'}
+      const component = getWrapper(props)
       expect(component.find(SectionEmbed).length).toBe(1)
     })
 
     it('Can render an image section', () => {
-      props.section = new Section({type: 'image'})
-      const component = mount(
-        <SectionContainer {...props} />
-      )
+      props.section = {type: 'image'}
+      const component = getWrapper(props)
       expect(component.find(SectionImages).length).toBe(1)
     })
 
     it('Can render an image_collection section', () => {
-      props.section = new Section({type: 'image_collection'})
-      const component = mount(
-        <SectionContainer {...props} />
-      )
+      props.section = {type: 'image_collection'}
+      const component = getWrapper(props)
       expect(component.find(SectionImages).length).toBe(1)
     })
 
     it('Can render an image_set section', () => {
-      props.section = new Section({type: 'image_set'})
-      const component = mount(
-        <SectionContainer {...props} />
-      )
+      props.section = {type: 'image_set'}
+      const component = getWrapper(props)
       expect(component.find(SectionImages).length).toBe(1)
     })
 
-    it('Can render a slideshow section', () => {
-      props.section = new Section({type: 'slideshow'})
-      const component = mount(
-        <SectionContainer {...props} />
-      )
+    xit('Can render a slideshow section', () => {
+      props.section = {type: 'slideshow'}
+      const component = getWrapper(props)
       expect(component.find(SectionSlideshow).length).toBe(1)
     })
 
     it('Can render a text section', () => {
-      props.section = new Section({type: 'text'})
-      const component = mount(
-        <SectionContainer {...props} />
-      )
+      props.section = {type: 'text'}
+      const component = getWrapper(props)
       expect(component.find(SectionText).length).toBe(1)
     })
 
-    xit('Can render a video section', () => {
-      props.section = new Section({type: 'video'})
-      const component = mount(
-        <SectionContainer {...props} />
-      )
+    it('Can render a video section', () => {
+      props.section = {type: 'video'}
+      const component = getWrapper(props)
       expect(component.find(SectionVideo).length).toBe(1)
     })
   })
 
   describe('Hero Section', () => {
     beforeEach(() => {
-      props.section = new Section({isHero: true})
+      props.isHero = true
     })
 
     it('Can render an image section', () => {
-      props.section.set({type: 'image_set'})
-      const component = mount(
-        <SectionContainer {...props} />
-      )
+      props.section.type = 'image_set'
+      const component = getWrapper(props)
       expect(component.find(SectionImages).length).toBe(1)
     })
 
-    xit('Can render a video section', () => {
-      props.section.set({type: 'video'})
-      const component = mount(
-        <SectionContainer {...props} />
-      )
+    it('Can render a video section', () => {
+      props.section.type = 'video'
+      const component = getWrapper(props)
       expect(component.find(SectionVideo).length).toBe(1)
     })
 
     it('Does not render drag icon', () => {
-      const component = mount(
-        <SectionContainer {...props} />
-      )
-      expect(component.find(IconDrag).length).toBe(1)
+      const component = getWrapper(props)
+      expect(component.find(IconDrag).length).toBe(0)
     })
 
     it('Can remove a hero section', () => {
-      const spy = jest.spyOn(props.section, 'destroy')
-      const component = mount(
-        <SectionContainer {...props} />
-      )
+      const component = getWrapper(props)
       component.find(RemoveButton).at(0).simulate('click')
-      expect(spy).toHaveBeenCalled()
+
+      expect(props.onRemoveHero).toBeCalled()
     })
   })
 })
