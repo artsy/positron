1abfc9fddb00b48c634a7b3699b0c166df541474
Luc Succes <l2succes@gmail.com>
2018-02-19 09:45:24 -0500

    [Dev] remove comments

--- api/apps/sessions/index.js
+++ api/apps/sessions/index.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import { index, saveSession, deleteSession } from './routes'
-// import { setUser, authenticated, adminOnly } from '../users/routes'
 
 const app = module.exports = express()
 
