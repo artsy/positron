07aea1e76150d06f78669a4e7661bafcef8f1082
Eve Essex <eve.essex@gmail.com>
2018-03-06 19:19:46 -0500

    article publish date component

---
+++ client/apps/edit2/components/admin/components/article/article_publish_date.jsx
@@ -0,0 +1,168 @@
+import colors from '@artsy/reaction/dist/Assets/Colors'
+import moment from 'moment'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { Fonts } from '@artsy/reaction/dist/Components/Publishing/Fonts'
+
+export class ArticlePublishDate extends Component {
+  static propTypes = {
+    article: PropTypes.object,
+    onChange: PropTypes.func
+  }
+
+  state = {
+    focus: false,
+    hasChanged: false,
+    publish_date: null,
+    publish_time: null
+  }
+
+  componentWillMount = () => {
+    this.setState(this.setupPublishDate())
+  }
+
+  onScheduleChange = () => {
+    const { article, onChange } = this.props
+
+    if (this.date && this.time) {
+      const date = this.date.value
+      const time = this.time.value
+      const new_published = moment(`${date} ${time}`).local()
+
+      if (!article.published) {
+        onChange('scheduled_publish_at', new_published.toISOString())
+      } else {
+        onChange('published_at', new_published.toISOString())
+      }
+    }
+  }
+
+  setupPublishDate = () => {
+    const { published_at, scheduled_publish_at } = this.props.article
+    let publish_date
+    let publish_time
+
+    if (scheduled_publish_at) {
+      publish_date = moment(scheduled_publish_at).format('YYYY-MM-DD')
+      publish_time = moment(scheduled_publish_at).format('HH:mm')
+    } else if (published_at) {
+      publish_date = moment(published_at).format('YYYY-MM-DD')
+      publish_time = moment(published_at).format('HH:mm')
+    } else {
+      publish_date = moment().local().format('YYYY-MM-DD')
+      publish_time = moment().local().format('HH:mm')
+    }
+
+    return { publish_date, publish_time }
+  }
+
+  getPublishText = () => {
+    const { published, scheduled_publish_at } = this.props.article
+
+    if (published) {
+      return 'Update'
+    } else if (scheduled_publish_at) {
+      return 'Unschedule'
+    } else {
+      return 'Schedule'
+    }
+  }
+
+  onChangeFocus = () => {
+    if (this.date && this.time) {
+      const dateIsFocused = document.activeElement === ReactDOM.findDOMNode(this.date)
+      const timeIsFocused = document.activeElement === ReactDOM.findDOMNode(this.time)
+      const hasChanged = this.hasChanged()
+
+      this.setState({
+        hasChanged,
+        focus: (dateIsFocused || timeIsFocused)
+      })
+    }
+  }
+
+  hasChanged = () => {
+    const { publish_date, publish_time } = this.state
+
+    if (this.date && this.time) {
+      const dateHasChanged = publish_date !== this.date.value
+      const timeHasChanged = publish_time !== this.time.value
+
+      return dateHasChanged || timeHasChanged
+    }
+  }
+
+  render () {
+    const { focus, hasChanged, publish_date, publish_time } = this.state
+
+    return (
+      <div className='ArticlePublishDate'>
+        <label>Publish Date/Time</label>
+
+        <DateContainer>
+          <InputGroup
+            focus={focus}
+            onClick={this.onChangeFocus}
+            onMouseUp={this.onChangeFocus}
+            onKeyUp={this.onChangeFocus}
+          >
+            <input
+              className='bordered-input'
+              defaultValue={publish_date}
+              type='date'
+              ref={(ref) => { this.date = ref }}
+              onBlur={this.onChangeFocus}
+            />
+            <input
+              className='bordered-input'
+              defaultValue={publish_time}
+              type='time'
+              ref={(ref) => { this.time = ref }}
+              onBlur={this.onChangeFocus}
+            />
+          </InputGroup>
+
+          <ButtonMedium
+            background={hasChanged ? colors.redMedium : undefined}
+            onClick={this.onScheduleChange}
+          >
+            {this.getPublishText()}
+          </ButtonMedium>
+        </DateContainer>
+      </div>
+    )
+  }
+}
+export const DateContainer = styled.div`
+  display: flex;
+  margin: 5px 0 40px 0;
+`
+
+export const InputGroup = styled.div`
+  display: flex;
+  border: 2px solid ${props => props.focus ? colors.purpleRegular : colors.grayRegular};
+  transition: border .3s;
+  margin-right: 20px;
+  .bordered-input {
+    border: none;
+    margin-top: 0;
+  }
+`
+
+export const ButtonMedium = styled.button`
+  padding: 10px 20px;
+  color: ${props => props.color ? props.color : 'white'};
+  cursor: pointer;
+  background: ${props => props.background ? props.background : colors.grayMedium};
+  ${Fonts.avantgarde('s11')}
+  outline: none;
+  border: none;
+  width: -moz-available;
+  width: -webkit-fill-available;
+  transition: background .3s;
+  &:hover {
+    background: ${colors.purpleRegular};
+  }
+`
---
+++ client/apps/edit2/components/admin/test/components/article/article_publish_date.test.js
@@ -0,0 +1,200 @@
+import moment from 'moment'
+import { cloneDeep } from 'lodash'
+import { mount } from 'enzyme'
+import React from 'react'
+import { Fixtures } from '@artsy/reaction/dist/Components/Publishing'
+import { ArticlePublishDate } from '../../../components/article/article_publish_date'
+
+describe('ArticlePublishDate', () => {
+  let props
+
+  const getWrapper = (props) => {
+    return mount(
+      <ArticlePublishDate {...props} />
+    )
+  }
+
+  beforeEach(() => {
+    let article = cloneDeep(Fixtures.StandardArticle)
+
+    props = {
+      article,
+      onChange: jest.fn()
+    }
+  })
+
+  it('Renders a label', () => {
+    const component = getWrapper(props)
+    expect(component.text()).toMatch('Publish Date/Time')
+  })
+
+  describe('date', () => {
+    it('Can render a date input with current date as default', () => {
+      props.article.published = false
+      delete props.article.published_at
+      const component = getWrapper(props)
+
+      expect(component.find('input[type="date"]').getElement().props.defaultValue).toBe(
+        moment().local().format('YYYY-MM-DD')
+      )
+    })
+
+    it('Can render a date input with saved published_at', () => {
+      const component = getWrapper(props)
+
+      expect(component.find('input[type="date"]').getElement().props.defaultValue).toBe(
+        moment(props.article.published_at).format('YYYY-MM-DD')
+      )
+    })
+
+    it('Can render a date input with saved scheduled_published_at', () => {
+      const date = moment().add('1', 'days').toISOString()
+      props.article.scheduled_publish_at = date
+      delete props.article.published_at
+      const component = getWrapper(props)
+
+      expect(component.find('input[type="date"]').getElement().props.defaultValue).toBe(
+        moment(props.article.scheduled_publish_at).format('YYYY-MM-DD')
+      )
+    })
+
+    it('Can change the date', () => {
+      props.article.published = true
+      const component = getWrapper(props)
+      component.instance().date.value = '2018-05-04'
+      component.instance().onScheduleChange()
+
+      expect(props.onChange.mock.calls[0][0]).toBe('published_at')
+      expect(props.onChange.mock.calls[0][1]).toMatch('2018-05-04')
+    })
+  })
+
+  describe('time', () => {
+    it('Can render a time input with current time as default', () => {
+      props.article.published = false
+      delete props.article.published_at
+      const component = getWrapper(props)
+
+      expect(component.find('input[type="time"]').getElement().props.defaultValue).toBe(
+        moment().local().format('HH:m')
+      )
+    })
+
+    it('Can render a time input with saved published_at', () => {
+      const component = getWrapper(props)
+
+      expect(component.find('input[type="time"]').getElement().props.defaultValue).toBe(
+        moment(props.article.published_at).format('HH:mm')
+      )
+    })
+
+    it('Can render a time input with saved scheduled_published_at', () => {
+      const date = moment().add('1', 'days').toISOString()
+      props.article.scheduled_publish_at = date
+      delete props.article.published_at
+      const component = getWrapper(props)
+
+      expect(component.find('input[type="time"]').getElement().props.defaultValue).toBe(
+        moment(props.article.scheduled_publish_at).format('HH:mm')
+      )
+    })
+
+    it('Can change the time', () => {
+      props.article.published = true
+      const component = getWrapper(props)
+      component.instance().time.value = '02:34'
+      component.instance().onScheduleChange()
+
+      expect(props.onChange.mock.calls[0][0]).toBe('published_at')
+      // Not sure why this isn't matching above, maybe looking for local?
+      expect(props.onChange.mock.calls[0][1]).toMatch('06:34')
+    })
+  })
+
+  describe('#setupPublishDate', () => {
+    it('Returns expected date and time if published_at', () => {
+      const component = getWrapper(props)
+      const setupDate = component.instance().setupPublishDate()
+
+      expect(setupDate.publish_date).toBe(moment(props.article.published_at).format('YYYY-MM-DD'))
+      expect(setupDate.publish_time).toBe(moment(props.article.published_at).format('HH:mm'))
+    })
+
+    it('Returns expected date and time if scheduled_published_at', () => {
+      const date = moment().add('1', 'days').toISOString()
+      props.article.scheduled_publish_at = date
+      delete props.article.published_at
+      const component = getWrapper(props)
+      const setupDate = component.instance().setupPublishDate()
+
+      expect(setupDate.publish_date).toBe(moment(date).format('YYYY-MM-DD'))
+      expect(setupDate.publish_time).toBe(moment(date).format('HH:mm'))
+    })
+
+    it('Returns current date if no saved dates', () => {
+      props.article.published = false
+      delete props.article.published_at
+      const component = getWrapper(props)
+      const setupDate = component.instance().setupPublishDate()
+
+      expect(setupDate.publish_date).toBe(moment().local().format('YYYY-MM-DD'))
+    })
+  })
+
+  describe('#getPublishText', () => {
+    it('Returns "Update" if published', () => {
+      props.article.published = true
+      const component = getWrapper(props)
+      const text = component.instance().getPublishText()
+
+      expect(text).toBe('Update')
+    })
+    it('Returns "Schedule" if unpublished', () => {
+      const component = getWrapper(props)
+      const text = component.instance().getPublishText()
+
+      expect(text).toBe('Schedule')
+    })
+
+    it('Returns "Unschedule" if unpublished and scheduled_published_at', () => {
+      const date = moment().add('1', 'days').toISOString()
+      props.article.scheduled_publish_at = date
+      const component = getWrapper(props)
+      const text = component.instance().getPublishText()
+
+      expect(text).toBe('Unschedule')
+    })
+  })
+
+  it('#hasChanged returns true if state date and time do not match inputs', () => {
+    const component = getWrapper(props)
+    component.instance().date.value = '2018-02-02'
+    component.instance().time.value = '02:34'
+    const hasChanged = component.instance().hasChanged()
+
+    expect(hasChanged).toBe(true)
+  })
+
+  it('#onChangeFocus sets state if focus has changed', () => {
+    const component = getWrapper(props)
+    component.instance().setState = jest.fn()
+    component.update()
+    component.instance().date.value = '2018-02-02'
+    component.instance().time.value = '02:34'
+    component.instance().onChangeFocus()
+
+    expect(component.instance().setState.mock.calls[0][0].hasChanged).toBe(true)
+    expect(component.instance().setState.mock.calls[0][0].focus).toBe(false)
+  })
+
+  it('Button calls #onScheduleChange on click', () => {
+    const date = moment().add('1', 'days').toISOString()
+    props.article.scheduled_publish_at = date
+    delete props.article.published_at
+    const component = getWrapper(props)
+    component.find('button').at(0).simulate('click')
+
+    expect(props.onChange.mock.calls[0][0]).toBe('scheduled_publish_at')
+    expect(props.onChange.mock.calls[0][1]).toMatch(moment(date).format('YYYY-MM-'))
+  })
+})
