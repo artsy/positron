f6e504f4231735bf417f1cd21337d059dbba9873
Eve Essex <eve.essex@gmail.com>
2018-03-14 14:56:25 -0400

    can save session if no username

--- api/apps/sessions/test/model.test.js
+++ api/apps/sessions/test/model.test.js
@@ -34,6 +34,24 @@ describe('Sessions Model', () => {
     })
   })
 
+  it('Can #save without username (partners)', (done) => {
+    save({
+      _id: '59fa424574132b001d917253',
+      id: '59fa424574132b001d917253',
+      timestamp: '2018-02-20T21:13:09.358Z',
+      user: { id: '586ff4069c18db5923002ca6' },
+      article: '59fa424574132b001d917253',
+      channel:
+      { id: '5759e3efb5989e6f98f77993',
+        name: 'Artsy Editorial',
+        type: 'editorial'
+      }
+    }, (_, session) => {
+      session._id.should.equal('59fa424574132b001d917253')
+      done()
+    })
+  })
+
   it('#destroy', (done) => {
     where({}, (_, data) => {
       data.length.should.equal(3)
