aa2ddfd94c41789c4ca4f100d958699e83855714
Eve Essex <eve.essex@gmail.com>
2018-06-05 18:33:16 -0400

    dont extend editActions

--- src/client/actions/edit/index.js
+++ src/client/actions/edit/index.js
@@ -1,13 +1,11 @@
-
-import { extend } from 'lodash'
 import { actions as articleActions } from './articleActions'
 import { actions as editActions } from './editActions'
 import { actions as errorActions } from './errorActions'
 import { actions as sectionActions } from './sectionActions'
 
-export const actions = extend({},
-  articleActions,
-  editActions,
-  errorActions,
-  sectionActions
-)
+export const actions = {
+  ...articleActions,
+  ...editActions,
+  ...errorActions,
+  ...sectionActions
+}
