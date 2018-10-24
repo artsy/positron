9bbac829f48f8a4c2913ad5e4a4b84114b838c6f
Eve Essex <eve.essex@gmail.com>
2018-10-24 12:15:36 -0400

    use styled components for SectionTool

--- src/client/apps/edit/components/content/section_tool/index.jsx
+++ src/client/apps/edit/components/content/section_tool/index.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components"
 import PropTypes from "prop-types"
 import React, { Component } from "react"
 import { connect } from "react-redux"
+import { color } from "@artsy/palette"
+import { avantgarde } from "@artsy/reaction/dist/Assets/Fonts"
 import { IconEditEmbed } from "@artsy/reaction/dist/Components/Publishing/Icon/IconEditEmbed"
 import { IconEditImages } from "@artsy/reaction/dist/Components/Publishing/Icon/IconEditImages"
 import { IconEditSection } from "@artsy/reaction/dist/Components/Publishing/Icon/IconEditSection"
@@ -27,47 +29,42 @@ export class SectionTool extends Component {
   }
 
   state = {
-    open: false,
+    isOpen: false,
   }
 
   toggleOpen = () => {
-    this.setState({ open: !this.state.open })
+    this.setState({ isOpen: !this.state.isOpen })
   }
 
   newSection = type => {
     const { index, newSectionAction } = this.props
 
     newSectionAction(type, index + 1)
-    this.setState({ open: false })
+    this.setState({ isOpen: false })
   }
 
   setHero = type => {
     const { newHeroSectionAction, onSetEditing } = this.props
 
     newHeroSectionAction(type)
-    this.setState({ open: false })
+    this.setState({ isOpen: false })
     onSetEditing(true)
   }
 
   renderHeroMenu() {
-    if (this.state.open) {
+    if (this.state.isOpen) {
       return (
-        <ul className="edit-tool__menu">
-          <li
-            className="edit-tool__hero-image"
-            onClick={() => this.setHero("image_collection")}
-          >
+        <SectionToolMenu>
+          <SectionToolMenuItem onClick={() => this.setHero("image_collection")}>
             <IconHeroImage />
             Large Format Image
-          </li>
-          <li
-            className="edit-tool__hero-video"
-            onClick={() => this.setHero("video")}
-          >
+          </SectionToolMenuItem>
+
+          <SectionToolMenuItem onClick={() => this.setHero("video")}>
             <IconHeroVideo />
             Large Format Video
-          </li>
-        </ul>
+          </SectionToolMenuItem>
+        </SectionToolMenu>
       )
     }
   }
@@ -78,84 +75,76 @@ export class SectionTool extends Component {
       firstSection,
       isPartnerChannel,
     } = this.props
-    const { open } = this.state
+    const { isOpen } = this.state
     const isNews = layout === "news"
 
-    if (open) {
+    if (isOpen) {
       return (
-        <ul className="edit-tool__menu">
-          <li
-            className="edit-tool__edit-text"
-            onClick={() => this.newSection("text")}
-          >
+        <SectionToolMenu>
+          <SectionToolMenuItem onClick={() => this.newSection("text")}>
             <IconEditText />
             Text
-          </li>
-          <li
-            className="edit-tool__edit-images"
-            onClick={() => this.newSection("image_collection")}
-          >
+          </SectionToolMenuItem>
+
+          <SectionToolMenuItem onClick={() => this.newSection("image_collection")}>
             <IconEditImages />
             {isNews ? "Image" : "Images"}
-          </li>
-          {!isNews && (
-            <li
-              className="edit-tool__edit-video"
-              onClick={() => this.newSection("video")}
-            >
+          </SectionToolMenuItem>
+
+          {!isNews &&
+            <SectionToolMenuItem onClick={() => this.newSection("video")}>
               <IconEditVideo />
               Video
-            </li>
-          )}
-          {!isPartnerChannel &&
-            !isNews && (
-              <li
-                className="edit-tool__edit-embed"
-                onClick={() => this.newSection("embed")}
-              >
-                <IconEditEmbed />
-                Embed
-              </li>
-            )}
-          {isNews &&
-            !firstSection && (
-              <li
-                className="edit-tool__edit-socialembed"
-                onClick={() => this.newSection("social_embed")}
-              >
-                <IconEditEmbed />
-                Social Embed
-              </li>
-            )}
-        </ul>
+            </SectionToolMenuItem>
+          }
+
+          {!isPartnerChannel && !isNews &&
+            <SectionToolMenuItem onClick={() => this.newSection("embed")}>
+              <IconEditEmbed />
+              Embed
+            </SectionToolMenuItem>
+          }
+
+          {isNews && !firstSection &&
+            <SectionToolMenuItem onClick={() => this.newSection("social_embed")}>
+              <IconEditEmbed />
+              Social Embed
+            </SectionToolMenuItem>
+          }
+        </SectionToolMenu>
       )
     }
   }
 
   render() {
     const { article, firstSection, index, isEditing, isHero, sections } = this.props
-    const { open } = this.state
+    const { isOpen } = this.state
 
     const isFirstSection = sections && firstSection && sections.length === 0
     const isLastSection = sections && index === sections.length - 1
     const sectionWidth = getSectionWidth(null, article.layout)
+    const isVisible = isFirstSection || isLastSection || isHero
 
     return (
       <SectionToolContainer
-        className={"edit-tool"}
-        data-state-open={open}
-        data-editing={isEditing}
-        data-visible={isFirstSection || isLastSection}
-        data-hero={isHero}
         isEditing={isEditing}
+        isOpen={isOpen}
+        isHero={isHero}
+        isVisible={isVisible}
         width={sectionWidth}
       >
-        <div className="edit-tool__icon" onClick={this.toggleOpen}>
+        <SectionToolIcon
+          width={sectionWidth}
+          isHero={isHero}
+          isVisible={isVisible}
+          isOpen={isOpen}
+          onClick={this.toggleOpen}
+        >
           <IconEditSection
-            fill={open || !isHero ? "#000" : "#CCC"}
-            isClosing={open}
+            fill={isOpen || !isHero ? "#000" : color("black10")}
+            isClosing={isOpen}
           />
-        </div>
+        </SectionToolIcon>
 
         {isHero ? this.renderHeroMenu() : this.renderSectionMenu()}
       </SectionToolContainer>
@@ -186,7 +175,117 @@ const SectionToolContainer = styled.div`
   margin-right: auto;
   max-width: ${props => props.width ? props.width : "100%"};
   width: 100%;
+  transition: max-height 0.15s linear, opacity 0.15s linear;
+
   ${props => props.isEditing && `
     z-index: -1;
   `}
+  &:last-child {
+    opacity: 1;
+    margin-top: 15px;
+  }
+  ${props => props.isOpen && `
+    max-height: inherit;
+  `}
+  ${props => (props.isVisible && !props.isHero) && `
+    padding-bottom: 40px;
+  `}
+`
+
+export const SectionToolIcon = styled.div`
+  ${avantgarde("s13")};
+  position: relative;
+  cursor: pointer;
+  top: -6px;
+  opacity: 0;
+  transition: opacity 0.15s ease-out;
+
+  svg {
+    z-index: 1;
+    width: 40px;
+    height: 40px;
+    position: absolute;
+    left: -18px;
+    top: -13px;
+  }
+  path {
+    transition: fill 0.1s;
+  }
+  &::before {
+    content: '.';
+    border-top: 2px solid black;
+    position: absolute;
+    top: 6px;
+    left: 16px;
+    max-width: 0;
+    width: calc(${props => props.width} - 16px);
+    color: transparent;
+    opacity: 0;
+    transition: opacity 0.15s ease-out, max-width 0.15s ease-out;
+    height: 20px;
+    z-index: 2;
+  }
+  ${props => (props.isVisible || props.isOpen) && `
+    opacity: 1;
+  `}
+  ${props => (props.isVisible && !props.isOpen && !props.isHero) && `
+    &::after {
+      content: 'Add a section';
+      width: 150px;
+      display: block;
+      float: left;
+      position: absolute;
+      top: -3px;
+      left: 35px;
+    }
+  `}
+  &:hover {
+    opacity: 1;
+    path {
+      fill: black;
+    }
+    &::before {
+      max-width: 100%;
+      opacity: 1;
+    }
+    &::after {
+      display: none;
+    }
+  }
+`
+
+const SectionToolMenu = styled.ul`
+  display: flex;
+  border: 2px solid black;
+  position: relative;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+`
+
+const SectionToolMenuItem = styled.li`
+  ${avantgarde("s11")};
+  background: white;
+  height: 89px;
+  border-right: 1px solid ${color("black10")};
+  border-bottom: 1px solid ${color("black10")};
+  vertical-align: top;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  position: relative;
+
+  &:last-child {
+    border-right: none;
+  }
+
+  svg {
+    max-width: 40px;
+    max-height: 35px;
+    height: 35px;
+    margin-bottom: 5px;
+  }
 `
