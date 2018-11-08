970056010167871859138476e4fd12abfc9875f0
Isac Petruzzi <isac.petruzzi@gmail.com>
2018-11-08 17:31:54 +0100

    add --detectOpenHandles flag to jest

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,4 @@ set -e -x
 yarn mocha $(find src/api -name '*.test.coffee')
 yarn mocha $(find src/api -name '*.spec.js')
 yarn mocha $(find src/client -name '*.test.coffee')
-yarn jest --maxWorkers=2 --forceExit
+yarn jest --maxWorkers=2 --detectOpenHandles --forceExit
