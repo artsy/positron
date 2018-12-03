b97e20dd7643ece8c10ce22ecb1a662d0f67323f
Lillan Pace <lillanpace@Lillan-Pace.local>
2018-12-03 16:24:01 -0500

    setup ui

--- src/client/apps/edit/components/header/yoast/index.tsx
+++ src/client/apps/edit/components/header/yoast/index.tsx
@@ -1,6 +1,7 @@
-import { color, Flex, space } from "@artsy/palette"
-import { StaticCollapse } from "@artsy/reaction/dist/Components"
+import { Box, color, Flex, Separator, space } from "@artsy/palette"
+import { Input } from "@artsy/reaction/dist/Components/Input"
 import { ArticleData } from "@artsy/reaction/dist/Components/Publishing/Typings"
+import { StaticCollapse } from "@artsy/reaction/dist/Components/StaticCollapse"
 import React, { Component } from "react"
 import styled from "styled-components"
 
@@ -17,7 +18,7 @@ interface State {
 
 export class Yoast extends Component<Props, State> {
   state = {
-    isOpen: true,
+    isOpen: false,
   }
 
   toggleDrawer = () => {
@@ -26,26 +27,36 @@ export class Yoast extends Component<Props, State> {
   render() {
     const { isOpen } = this.state
     return (
-      <YoastContainer>
-        <Flex onClick={this.toggleDrawer}>
+      <Box>
+        <YoastContainer onClick={this.toggleDrawer}>
           Seo Analysis - <span>Set Target Keyword</span>
-        </Flex>
+        </YoastContainer>
         <StaticCollapse open={isOpen}>
-          <Drawer>HELLO</Drawer>
+          <Drawer>
+            <Box width={[1, 1 / 3]}>
+              <Input
+                title="Target Keyword"
+                placeholder="A searchable term for this content"
+              />
+            </Box>
+            <Flex width={[1, 2 / 3]}>
+              <Separator width={2} />
+              kjldsfkjsdfl;sfl;ef
+            </Flex>
+          </Drawer>
         </StaticCollapse>
-      </YoastContainer>
+      </Box>
     )
   }
 }
 
-export const YoastContainer = styled(Flex)`
+export const YoastContainer = styled(Box)`
   background-color: ${color("black5")};
   padding: 0 20px;
-  height: 40px;
+  height: ${space(4)}px;
 `
 
 export const Drawer = styled(Flex)`
   background-color: ${color("white100")};
   padding: 0 20px;
-  height: ${space(4)}px;
 `
