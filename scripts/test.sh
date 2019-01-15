6b73264ad1ed5185e997b051bf60484263e2142a
Daniel Levenson <dleve123@gmail.com>
2019-01-15 16:49:11 -0500

    Revert "Downgrade coveralls to 2.12"
    This reverts commit 04dee17de537d10bf73cdbd8d51e3080210e4326.

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,4 @@ set -e -x
 yarn mocha $(find src/api -name '*.test.coffee')
 yarn mocha $(find src/api -name '*.spec.js')
 yarn mocha $(find src/client -name '*.test.coffee')
-yarn nyc jest --maxWorkers=2 --detectOpenHandles --forceExit --collectCoverageFrom=src/**/*.js --collectCoverageFrom=!src/client/collections
+yarn jest --maxWorkers=2 --detectOpenHandles --forceExit --collectCoverageFrom=src/**/*.js --collectCoverageFrom=!src/client/collections --coverage
