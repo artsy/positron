ae5bcefe1bb9531818c31fc3650df297a166ecf1
Daniel Levenson <dleve123@gmail.com>
2019-01-15 15:40:16 -0500

    Remove custom placement of jest coverage data

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,4 @@ set -e -x
 yarn mocha $(find src/api -name '*.test.coffee')
 yarn mocha $(find src/api -name '*.spec.js')
 yarn mocha $(find src/client -name '*.test.coffee')
-yarn jest --maxWorkers=2 --detectOpenHandles --forceExit --coverage --coverageDirectory .nyc_output --coverageReporters json
+yarn jest --maxWorkers=2 --detectOpenHandles --forceExit --coverage
