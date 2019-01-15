24ac93f371bbfd50d87f38ef4df3afc9789674d1
Daniel Levenson <dleve123@gmail.com>
2019-01-15 16:30:26 -0500

    Try ignoring src/client/collections from coverage collection

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,4 @@ set -e -x
 yarn mocha $(find src/api -name '*.test.coffee')
 yarn mocha $(find src/api -name '*.spec.js')
 yarn mocha $(find src/client -name '*.test.coffee')
-yarn jest --maxWorkers=2 --detectOpenHandles --forceExit --coverage
+yarn nyc jest --maxWorkers=2 --detectOpenHandles --forceExit --collectCoverageFrom=src/**/*.js --collectCoverageFrom=!src/client/collections
