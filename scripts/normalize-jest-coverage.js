dcec5f0538cc365281e9936b199077ad63659bbd
Kieran Gillen <kgillen@gmail.com>
2019-01-23 11:53:03 -0500

    Adds prettier formatting to normalize jest coverage

--- scripts/normalize-jest-coverage.js
+++ scripts/normalize-jest-coverage.js
@@ -1,28 +1,27 @@
-const libCoverage = require('istanbul-lib-coverage');
-const { createReporter } = require('istanbul-api');
-const fs = require('fs')
-const path = require('path')
+const libCoverage = require("istanbul-lib-coverage")
+const { createReporter } = require("istanbul-api")
+const fs = require("fs")
+const path = require("path")
 
-const normalizeJestCoverage = (result) => {
-
+const normalizeJestCoverage = result => {
   Object.entries(result).forEach(([k, v]) => {
-    if (v.data) result[k] = v.data;
-  });
+    if (v.data) result[k] = v.data
+  })
 
-  return result;
-};
+  return result
+}
 
-const map = libCoverage.createCoverageMap();
-const jestCoverage = require('../.nyc_output/coverage-final.json');
+const map = libCoverage.createCoverageMap()
+const jestCoverage = require("../.nyc_output/coverage-final.json")
 
-map.merge(normalizeJestCoverage(jestCoverage));
+map.merge(normalizeJestCoverage(jestCoverage))
 
-const reporter = createReporter();
-reporter.addAll(['json']);
-reporter.write(map);
+const reporter = createReporter()
+reporter.addAll(["json"])
+reporter.write(map)
 
 fs.copyFile(
-  './coverage/coverage-final.json',
-  './.nyc_output/coverage-final.json',
+  "./coverage/coverage-final.json",
+  "./.nyc_output/coverage-final.json",
   error => error && console.log(error)
 )
