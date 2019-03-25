5b3ca0be4788dd25dd77f74aaff28b9c63a6e172
Eve Essex <eve.essex@gmail.com>
2019-03-25 12:54:01 -0400

    add tests for appReducer

--- src/client/reducers/appReducer.ts
+++ src/client/reducers/appReducer.ts
@@ -1,9 +1,19 @@
 import { data as sd } from "sharify"
 
-// TODO: confirm attrs
-interface UserState {
+interface ChannelState {
   name?: string
   id: string
+  type?: string
+}
+
+interface UserState {
+  access_token?: string
+  channel_ids?: string[]
+  current_channel?: ChannelState
+  email?: string
+  id: string
+  name?: string
+  partner_ids?: string[]
   type?: string
 }
 
@@ -11,7 +21,7 @@ export interface AppState {
   apiURL: string
   appURL: string
   artsyURL: string
-  channel: any // TODO: add typing
+  channel: ChannelState
   forceURL: string
   isAdmin: boolean
   isEditorial: boolean
@@ -25,19 +35,21 @@ export interface AppState {
  * including user/channel and booleans based on user/channel types,
  * as well as commonly referenced app URLs
  */
-export const initialState: AppState = {
-  apiURL: sd.API_URL,
-  appURL: sd.APP_URL,
-  artsyURL: sd.ARTSY_URL,
-  channel: sd.CURRENT_CHANNEL,
-  forceURL: sd.FORCE_URL,
-  isAdmin: sd.USER.type === "Admin",
-  isEditorial: sd.CURRENT_CHANNEL.type === "editorial",
-  isPartnerChannel: sd.CURRENT_CHANNEL.type === "partner",
-  metaphysicsURL: sd.GRAPHQL_ENDPOINT,
-  user: sd.USER,
-}
+export const getInitialState = () =>
+  ({
+    apiURL: sd.API_URL,
+    appURL: sd.APP_URL,
+    artsyURL: sd.ARTSY_URL,
+    channel: sd.CURRENT_CHANNEL,
+    forceURL: sd.FORCE_URL,
+    isAdmin: sd.USER && sd.USER.type === "Admin",
+    isEditorial: sd.CURRENT_CHANNEL && sd.CURRENT_CHANNEL.type === "editorial",
+    isPartnerChannel:
+      sd.CURRENT_CHANNEL && sd.CURRENT_CHANNEL.type === "partner",
+    metaphysicsURL: sd.GRAPHQL_ENDPOINT,
+    user: sd.USER,
+  } as AppState)
 
-export const appReducer = (state = initialState, _action) => {
+export const appReducer = (state = getInitialState(), _action) => {
   return state
 }
