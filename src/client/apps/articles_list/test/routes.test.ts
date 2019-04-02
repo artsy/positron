dc3c5260037d32972feda62a5304b55443608dea
Eve Essex <eve.essex@gmail.com>
2019-04-02 17:56:07 -0400

    extend routes tests and convert to ts

--- src/client/apps/articles_list/test/routes.test.ts
+++ src/client/apps/articles_list/test/routes.test.ts
@@ -3,23 +3,32 @@ const fixtures = require("../../../../test/helpers/fixtures")
 const User = require("../../../models/user")
 
 jest.mock("../../websocket", () => ({
-  getSessionsForChannel: jest.fn((channel, cb) => cb()),
+  getSessionsForChannel: jest.fn((_channel, cb) => cb()),
 }))
 
-jest.mock("lokka", () => {
-  return {
-    Lokka: () => ({
-      query: jest.fn().mockResolvedValue({
+jest.mock("lokka", () => ({
+  Lokka: jest.fn().mockImplementation(() => ({
+    query: jest.fn().mockReturnValue(
+      Promise.resolve({
         articles: [{ id: "123" }],
-      }),
-    }),
+      })
+    ),
+  })),
+}))
+const LokkaMock = require("lokka").Lokka as jest.Mock
+
+jest.mock("lokka-transport-http", () => {
+  return {
+    Transport: jest.fn(),
   }
 })
+const TransportMock = require("lokka-transport-http").Transport as jest.Mock
 
 describe("routes", () => {
   let req
   let res
   let next
+  let queryMock
 
   beforeEach(() => {
     req = {
@@ -34,6 +43,14 @@ describe("routes", () => {
     }
 
     next = jest.fn()
+    queryMock = jest.fn().mockReturnValue(
+      Promise.resolve({
+        articles: [{ id: "123" }],
+      })
+    )
+    LokkaMock.mockImplementation(() => ({
+      query: queryMock,
+    }))
   })
 
   describe("articles list", () => {
@@ -47,6 +64,34 @@ describe("routes", () => {
       expect(res.render.mock.calls[0][0]).toBe("index")
       expect(res.render.mock.calls[0][1].articles[0].id).toBe("123")
       expect(res.render.mock.calls[0][1].current_channel.name).toBe("Editorial")
+    })
+
+    it("Sets up http headers correctly", async () => {
+      await articles_list(req, res, next)
+      expect(TransportMock.mock.calls[0][1].headers["X-Access-Token"]).toBe(
+        fixtures().users.access_token
+      )
+    })
+
+    it("Calls Lokka with the expected query", async () => {
+      await articles_list(req, res, next)
+      expect(queryMock.mock.calls[0][0]).toEqual(`
+    {
+      articles(published: true, channel_id: "4d8cd73191a5c50ce200002b"){
+        thumbnail_image
+        thumbnail_title
+        slug
+        published_at
+        published
+        scheduled_publish_at
+        id
+        channel_id
+        partner_channel_id
+        updated_at
+        layout
+      }
+    }
+  `)
     })
 
     describe("queries", () => {
@@ -66,6 +111,12 @@ describe("routes", () => {
         await articles_list(req, res, next)
         expect(res.locals.sd.HAS_PUBLISHED).toBe(true)
       })
+    })
+  })
+
+  describe("renderArticles", () => {
+    it("does a thing", () => {
+      expect(true).toBeTruthy()
     })
   })
 })
