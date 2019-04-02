96d6c947f366eafa87e4aa15066d60aa713c74ba
Eve Essex <eve.essex@gmail.com>
2019-04-02 09:41:24 -0400

    put tests back

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,8 @@
 
 set -e -x
 
-# yarn mocha $(find src/api -name '*.test.coffee')
-# yarn mocha $(find src/api -name '*.spec.js')
-# yarn mocha $(find src/client -name '*.test.coffee')
+yarn mocha $(find src/api -name '*.test.coffee')
+yarn mocha $(find src/api -name '*.spec.js')
+yarn mocha $(find src/client -name '*.test.coffee')
 yarn jest
 yarn publish-coverage
