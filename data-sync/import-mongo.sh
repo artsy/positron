d5a13d55faba608159299f0fa34bbfa276bef5d8
Isac Petruzzi <isac.petruzzi@gmail.com>
2019-04-04 10:41:11 +0200

    set standard AWS_ env vars from Positron's S3_ conventions

--- data-sync/import-mongo.sh
+++ data-sync/import-mongo.sh
@@ -3,7 +3,10 @@ set -ev
 start_datetime=$(date -u +"%D %T %Z")
 echo "[data import] Starting at $start_datetime"
 
+export AWS_ACCESS_KEY_ID=$S3_KEY
+export AWS_SECRET_ACCESS_KEY=$S3_SECRET
 aws s3 cp s3://artsy-data/positron/archive.tar.gz archive.tar.gz
+
 mongorestore --uri="$MONGOHQ_URL" --stopOnError --drop --gzip --archive=archive.tar.gz
 
 end_datetime=$(date -u +"%D %T %Z")
