5e25d0615858a8124235fba792237d3d7465cf28
Eve Essex <eve.essex@gmail.com>
2020-08-11 14:43:32 -0400

    simplify graphql integration tests

--- src/api/apps/graphql/test/integration.spec.js
+++ src/api/apps/graphql/test/integration.spec.js
@@ -5,7 +5,6 @@ import {
   RelatedArticlesCanvasQuery,
 } from "api/apps/graphql/test/queries"
 const { ObjectId } = require("mongojs")
-
 const app = require("../../../index.coffee")
 const {
   fabricate,
@@ -16,197 +15,143 @@ const {
 describe("graphql endpoint", () => {
   let server
 
-  beforeEach(done => {
+  before(done => {
+    server = app.listen(5000, () => {
+      done()
+    })
+  })
+
+  beforeEach((done) => {
     empty(() => {
-      server = app.listen(5000, () => {
-        done()
-      })
-    })
-  })
-
-  afterEach(() => {
-    server.close()
-  })
-
-  it("can get a list of published articles", done => {
-    fabricate(
-      "articles",
-      [
-        { title: "Flowers on Flowers", published: true },
-        { title: "Plants on Plants", published: true },
-      ],
-      (err, articles) => {
-        const query = `
-        {
-          articles(published: true) {
-            title
-          }
-        }
-      `
-        request
-          .post("http://localhost:5000/graphql")
-          .send({ query })
-          .end((err, res) => {
-            res.body.data.articles.length.should.equal(2)
-            res.body.data.articles[0].title.should.equal("Plants on Plants")
-            res.body.data.articles[1].title.should.equal("Flowers on Flowers")
-            done()
-          })
-      }
-    )
-  })
-
-  it("can get sections in an article", done => {
-    fabricate("articles", [fixtures.articles], (err, articles) => {
-      request
-        .post("http://localhost:5000/graphql")
-        .send({ query: ArticleSectionsQuery })
-        .end((err, res) => {
-          res.body.data.articles.length.should.equal(1)
-          res.body.data.articles[0].sections[1].type.should.equal(
-            "image_collection"
-          )
-          res.body.data.articles[0].sections[2].type.should.equal("text")
-          res.body.data.articles[0].sections[2].body.should.containEql(
-            "10. Lisson Gallery"
-          )
-          res.body.data.articles[0].sections[5].type.should.equal("video")
-          res.body.data.articles[0].sections[5].url.should.equal(
-            "http://youtu.be/yYjLrJRuMnY"
-          )
-          done()
-        })
-    })
-  })
-
-  it("can get authors in relatedArticles", done => {
-    fabricate(
-      "authors",
-      [{ _id: ObjectId("55356a9deca560a0137bb4ae"), name: "Kana" }],
-      (err, articles) => {
-        fabricate(
-          "articles",
-          [
+      fabricate(
+        "authors",
+        [{ _id: ObjectId("55356a9deca560a0137bb4ae"), name: "Kana" }],
+        (err, res) => {
+          if (err) { console.warn(err.message) }
+        fabricate("articles", [
+          fixtures.articles,
+          { published: false, ...fixtures.articles },
             {
-              title: "Top Ten Booths",
+              title: "Top Eleven Booths",
               published: true,
               _id: ObjectId("5c9d3c1aa4ba105ad8336956"),
               author_ids: [ObjectId("55356a9deca560a0137bb4ae")],
               channel_id: ObjectId("5aa99c11da4c00d6bc33a816"),
             },
             {
+              title: "Top Twelve Booths",
+              _id: ObjectId("5f32e33bd273fe3b7f0e3f00"),
               published: true,
               featured: true,
               vertical: {
                 name: "Culture",
                 id: ObjectId("55356a9deca560a0137bb4a7"),
               },
+              layout: "series",
               channel_id: ObjectId("5aa99c11da4c00d6bc33a816"),
               author_ids: [ObjectId("55356a9deca560a0137bb4ae")],
               related_article_ids: [ObjectId("5c9d3c1aa4ba105ad8336956")],
             },
-          ],
-          (err, articles) => {
-            request
-              .post("http://localhost:5000/graphql")
-              .send({ query: RelatedArticlesQuery })
-              .end((err, res) => {
-                res.body.data.articles.length.should.equal(2)
-                res.body.data.articles[0].relatedArticles[0].title.should.equal(
-                  "Top Ten Booths"
-                )
-                res.body.data.articles[0].relatedArticles[0].authors[0].name.should.equal(
-                  "Kana"
-                )
-                done()
-              })
-          }
-        )
-      }
-    )
+        ], () => {
+          done()
+        })
+      })
+    })
   })
+
+  after(() => {
+    server.close()
+  })
+
+  it("can get a list of published articles", done => {
+      const query = `
+        {
+          articles(published: true) {
+            title
+          }
+        }
+      `
+      request
+        .post("http://localhost:5000/graphql")
+        .send({ query })
+        .end((err, {body: { data: { articles } }}) => {
+          if (err) { console.warn(err.message) }
+          articles.length.should.equal(3)
+          articles[0].title.should.equal("Top Twelve Booths")
+          articles[1].title.should.equal("Top Eleven Booths")
+          articles[2].title.should.equal("Top Ten Booths")
+          done()
+        })
+      })
+
+  it("can get sections in an article", done => {
+      request
+        .post("http://localhost:5000/graphql")
+        .send({ query: ArticleSectionsQuery })
+        .end((err, {body: { data: { articles } }}) => {
+          if (err) { console.warn(err.message) }
+          articles[0].sections[1].type.should.equal(
+            "image_collection"
+          )
+          articles[0].sections[2].type.should.equal("text")
+          articles[0].sections[2].body.should.containEql(
+            "10. Lisson Gallery"
+          )
+          articles[0].sections[5].type.should.equal("video")
+          articles[0].sections[5].url.should.equal(
+            "http://youtu.be/yYjLrJRuMnY"
+          )
+          done()
+        })
+  })
+
+  it("can get authors in relatedArticles", done => {
+    request
+      .post("http://localhost:5000/graphql")
+      .send({ query: RelatedArticlesQuery })
+      .end((err, {body: { data: { articles } }}) => {
+        if (err) { console.warn(err.message) }
+        articles.length.should.equal(3)
+        articles[0].relatedArticles[0].title.should.equal(
+          "Top Eleven Booths"
+        )
+        articles[0].relatedArticles[0].authors[0].name.should.equal(
+          "Kana"
+        )
+        done()
+      })
+    })
 
   it("can get seriesArticle in relatedArticles", done => {
-    fabricate(
-      "articles",
-      [
-        {
-          title: "Top Ten Booths",
-          published: true,
-          layout: "feature",
-          _id: ObjectId("5c9d3c1aa4ba105ad8336956"),
-          author_ids: [ObjectId("55356a9deca560a0137bb4ae")],
-          channel_id: ObjectId("5aa99c11da4c00d6bc33a816"),
-        },
-        {
-          published: true,
-          featured: true,
-          vertical: {
-            name: "Culture",
-            id: ObjectId("55356a9deca560a0137bb4a7"),
-          },
-          _id: ObjectId("5ae762608cd4110038b40049"),
-          layout: "series",
-          channel_id: ObjectId("5aa99c11da4c00d6bc33a816"),
-          author_ids: [ObjectId("55356a9deca560a0137bb4ae")],
-          related_article_ids: [ObjectId("5c9d3c1aa4ba105ad8336956")],
-        },
-      ],
-      (err, articles) => {
-        request
-          .post("http://localhost:5000/graphql")
-          .send({ query: RelatedArticlesQuery })
-          .end((err, res) => {
-            res.body.data.articles.length.should.equal(2)
-            res.body.data.articles[0].relatedArticles[0].title.should.equal(
-              "Top Ten Booths"
-            )
-            res.body.data.articles[0].relatedArticles[0].seriesArticle.id.should.equal(
-              "5ae762608cd4110038b40049"
-            )
-            done()
-          })
-      }
-    )
-  })
+      request
+        .post("http://localhost:5000/graphql")
+        .send({ query: RelatedArticlesQuery })
+        .end((err, {body: { data: { articles } }}) => {
+          if (err) { console.warn(err.message) }
+          articles[0].relatedArticles[0].title.should.equal(
+            "Top Eleven Booths"
+          )
+          articles[0].relatedArticles[0].seriesArticle.id.should.equal(
+            "5f32e33bd273fe3b7f0e3f00"
+          )
+          done()
+        })
+      })
 
   it("can get authors in relatedArticlesCanvas", done => {
-    fabricate(
-      "authors",
-      [{ _id: ObjectId("55356a9deca560a0137bb4ae"), name: "Kana" }],
-      (err, articles) => {
-        fabricate(
-          "articles",
-          [
-            fixtures.articles,
-            {
-              published: true,
-              featured: true,
-              vertical: {
-                name: "Culture",
-                id: ObjectId("55356a9deca560a0137bb4a7"),
-              },
-              channel_id: ObjectId("5aa99c11da4c00d6bc33a816"),
-              author_ids: [ObjectId("55356a9deca560a0137bb4ae")],
-            },
-          ],
-          (err, articles) => {
-            request
-              .post("http://localhost:5000/graphql")
-              .send({ query: RelatedArticlesCanvasQuery })
-              .end((err, res) => {
-                res.body.data.articles.length.should.equal(2)
-                res.body.data.articles[1].relatedArticlesCanvas[0].title.should.equal(
-                  "Top Ten Booths"
-                )
-                res.body.data.articles[1].relatedArticlesCanvas[0].authors[0].name.should.equal(
-                  "Kana"
-                )
-                done()
-              })
-          }
-        )
-      }
-    )
-  })
-})
+      request
+        .post("http://localhost:5000/graphql")
+        .send({ query: RelatedArticlesCanvasQuery })
+        .end((err, {body: { data: { articles } }}) => {
+          if (err) { console.warn(err.message) }
+          articles.length.should.equal(3)
+          articles[1].relatedArticlesCanvas[0].title.should.equal(
+            "Top Twelve Booths"
+          )
+          articles[1].relatedArticlesCanvas[0].authors[0].name.should.equal(
+            "Kana"
+          )
+          done()
+        })
+      })
+    })
