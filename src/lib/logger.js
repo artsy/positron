08b172f21f0a8e0e5fb9793eeedba738f505c4f4
Ozzie Vasdi <29984068+ovasdi@users.noreply.github.com>
2023-01-31 17:59:38 -0500

    refactor: expand redact paths

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -5,7 +5,25 @@ const httpLogger = require("pino-http")({
   },
 
   redact: {
-    paths: ['req.headers["x-access-token"]', "req.headers.cookie"],
+    paths: [
+      'req.headers["x-access-token"]',
+      "req.headers.cookie",
+      "req.headers.connection",
+      'req.headers["sec-ch-ua"]',
+      'req.headers["sec-ch-ua-mobile"]',
+      'req.headers["sec-ch-ua-platform"]',
+      'req.headers["upgrade-insecure-requests"]',
+      'req.headers["sec-fetch-site"]',
+      'req.headers["sec-fetch-mode"]',
+      'req.headers["sec-fetch-user"]',
+      'req.headers["sec-fetch-dest"]',
+      'req.headers["if-none-match"]',
+      'req.headers["x-datadog-trace-id"]',
+      'req.headers["x-datadog-parent-id"]',
+      'req.headers["x-datadog-sampling-priority"]',
+      "res.headers.etag",
+      'res.headers["access-control-allow-origin"]',
+    ],
     remove: true,
   },
 
