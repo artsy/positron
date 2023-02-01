6495ce7f4d13d057ac61478f777e98b76f61324a
Ozzie Vasdi <29984068+ovasdi@users.noreply.github.com>
2023-02-01 13:23:53 -0500

    refactor: logger options

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,21 +1,17 @@
 import pino from "pino"
 const httpLogger = require("pino-http")({
-  base: {
-    pid: undefined,
-  },
-
-  timestamp: pino.stdTimeFunctions.isoTime,
-
   // overwrite the req and res obejct specifying properties to keep
   serializers: {
     req(req) {
       return {
         method: req.method,
         url: req.url,
-        hostname: req.headers.host,
+        host: req.headers.host,
+        params: req.params,
+        query: req.query,
         useragent: req.headers["user-agent"],
         accept: req.headers.accept,
-        remoteAddress: req.remoteAddress,
+        address: req.remoteAddress,
         port: req.remotePort,
       }
     },
